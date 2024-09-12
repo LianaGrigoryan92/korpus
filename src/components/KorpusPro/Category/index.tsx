cb6dc9044177e4dd2aaebf10e5c398de7d239933
armen <arakelyan2001@mail.ru>
2024-09-12 23:37:21 +0400

    add iframe link

--- src/components/KorpusPro/Category/index.tsx
+++ src/components/KorpusPro/Category/index.tsx
@@ -2,6 +2,7 @@
 
 import { KorpusProModal } from '@/components';
 import { useAppDispatch } from '@/store/hooks';
+import { useState } from 'react';
 import { showModal } from '@/features';
 
 // styles & images
@@ -12,30 +13,47 @@ import KorpusmenCategoryImg from '@/public/images/korpus-pro/categories/korpusme
 
 export default function Category() {
   const dispatch = useAppDispatch();
+  const [isServiceIframeOpen, setServiceIframeOpen] = useState(false);
 
   const handleModuleCategoryClick = () => {
     dispatch(showModal());
   };
 
+  const handleServiceCategoryClick = () => {
+    setServiceIframeOpen((prevState) => !prevState);
+  };
+
   return (
-    <S.CategoryWrapper>
-      <KorpusProModal />
-      <S.CategoryContainer>
-        <S.CategoryItems>
-          <S.CategoryItem $bgImage={ServiceCategoryImg.src}>
-            <span>Service</span>
+      <S.CategoryWrapper>
+        <KorpusProModal />
+        <S.CategoryContainer>
+          <S.CategoryItems>
+            <S.CategoryItem
+                $bgImage={ServiceCategoryImg.src}
+                onClick={handleServiceCategoryClick}
+            >
+              <span>Service</span>
+            </S.CategoryItem>
+            {isServiceIframeOpen && (
+                <iframe
+                    src="https://cloud.bazissoft.ru/cutting/ru/#/client/auth/login?user=8782"
+                    frameBorder="0"
+                    height="100%"
+                    width="100%"
+                    style={{ position: 'absolute', top: 0, left: 0, zIndex: 10 }}
+                />
+            )}
+            <S.CategoryItem
+                $bgImage={ModulesCategoryImg.src}
+                onClick={handleModuleCategoryClick}
+            >
+              <span>Modules</span>
+            </S.CategoryItem>
+          </S.CategoryItems>
+          <S.CategoryItem $bgImage={KorpusmenCategoryImg.src}>
+            <span>Korpusmen</span>
           </S.CategoryItem>
-          <S.CategoryItem
-            $bgImage={ModulesCategoryImg.src}
-            onClick={handleModuleCategoryClick}
-          >
-            <span>Modules</span>
-          </S.CategoryItem>
-        </S.CategoryItems>
-        <S.CategoryItem $bgImage={KorpusmenCategoryImg.src}>
-          <span>Korpusmen</span>
-        </S.CategoryItem>
-      </S.CategoryContainer>
-    </S.CategoryWrapper>
+        </S.CategoryContainer>
+      </S.CategoryWrapper>
   );
 }
